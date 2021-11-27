45e88a15a56cf7f1109be9249fc7f011a37abd60
Siju Jacob <sijujacob@Sijus-MacBook-Pro-2.local>
2021-11-26 23:26:28 -0500

    updated pool id

--- src/UserPool.js
+++ src/UserPool.js
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
 
 const poolData = {
-    UserPoolId: "us-east-1_WVVwFnHZE",
-    ClientId: "5s9arhgupgtm2e3qmb2gb46na0"
+    UserPoolId: "us-east-1_VjLDa8AlF",
+    ClientId: "4kqns3v03if3fdqftgi0v6cldu"
 }
 
 export default new CognitoUserPool(poolData);
